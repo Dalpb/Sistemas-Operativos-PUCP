badb32e0c57d9a7e2dabc7b8981c146a8a8d66d2
Diego Palomino <dpalomino032002@gmail.com>
2024-04-30 17:06:45 -0500

    fataProductConsumidor

---
+++ Lab4Go/ejercicio5/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	_ "fmt"
+	"sync"
+	_ "sync"
+	_ "time"
+)
+
+var (
+	ch1 chan string = make(chan string)
+	ch2 chan string = make(chan string)
+	ch3 chan string = make(chan string)
+	ch4 chan string = make(chan string)
+	wg  sync.WaitGroup
+	mu  sync.Mutex
+)
+
+func insert_A() {
+	ch1 <- "A"
+}
+func insert_B() {
+	ch2 <- "B"
+}
+
+// los AB se hacen independientes
+func insert_C() {
+	defer wg.Done()
+	fmt.Printf("C")
+	ch3 <- ""
+}
+func insert_D() {
+	defer wg.Done()
+
+	fmt.Printf("D")
+	ch3 <- ""
+}
+func insert_E() {
+	defer wg.Done()
+	<-ch3
+	fmt.Printf("E")
+}
+func insertAB() {
+	wg.Add(1)
+	go insert_A()
+	go insert_B()
+
+	select {
+	case msg1 := <-ch1:
+		fmt.Printf(msg1)
+		wg.Done()
+	case msg2 := <-ch2:
+		fmt.Printf(msg2)
+		wg.Done()
+	}
+	wg.Wait()
+}
+func main() {
+
+	for {
+		insertAB()
+		wg.Add(2)
+		go insert_C()
+		go insert_E()
+		wg.Wait()
+		insertAB()
+		insertAB()
+		wg.Add(2)
+		go insert_D()
+		go insert_E()
+		wg.Wait()
+	}
+}
