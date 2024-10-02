22c502343d2c4c8211236e0d4b13b1a873126614
Diego Alejandro Palomino <dpalomino032002@gmail.com>
2024-10-02 10:29:38 -0500

    secuencia.go resolved

--- Laboratorio4/2024-1/secuencias/secuencia.go
+++ Laboratorio4/2024-1/secuencias/secuencia.go
@@ -5,45 +5,97 @@ import (
 	"sync"
 )
 
-var wg sync.WaitGroup
+var (
+	wg  sync.WaitGroup
+	ch1 chan struct{} = make(chan struct{}, 1) //for admin
+	ch2 chan struct{} = make(chan struct{}, 1) //for admin
+	ch3 chan struct{} = make(chan struct{}, 1)
+	ch4 chan struct{} = make(chan struct{}, 1)
+	ch5 chan struct{} = make(chan struct{}, 1)
+	ch6 chan struct{} = make(chan struct{}, 1)
+	ch7 chan struct{} = make(chan struct{}, 1)
+	ch8 chan struct{} = make(chan struct{}, 1)
+	ch9 chan struct{} = make(chan struct{})
+)
 
 func worker1() {
 	for {
-		fmt.Printf("A")
+		//each loop goroutine will print only one letter
+		select {
+		case <-ch1:
+			//goes first
+			fmt.Printf("A")   //print
+			ch7 <- struct{}{} //call to B
+		case <-ch2:
+			//goes second
+			<-ch8             //wait
+			fmt.Printf("A")   //print
+			ch3 <- struct{}{} //call to C
+		}
+
 	}
 	wg.Done()
 }
 
 func worker2() {
 	for {
-		fmt.Printf("B")
+		select {
+		case <-ch1:
+			//goes second
+			<-ch7             //wait
+			fmt.Printf("B")   //print
+			ch3 <- struct{}{} //call to C
+		case <-ch2:
+			//goes first
+			fmt.Printf("B")   //print
+			ch8 <- struct{}{} //call to A
+		}
+
 	}
 	wg.Done()
 }
 
 func worker3() {
 	for {
+		<-ch3
 		fmt.Printf("C")
+		ch4 <- struct{}{}
 	}
 	wg.Done()
 }
 
 func worker4() {
 	for {
+		<-ch4
 		fmt.Printf("D")
+		ch5 <- struct{}{}
 	}
 	wg.Done()
 }
 
 func worker5() {
 	for {
-		fmt.Printf("E")
+		<-ch5
+		fmt.Printf("E\n")
+		//finished one loop
+		ch9 <- struct{}{}
 	}
 	wg.Done()
+}
+func admin() {
+	for {
+		ch1 <- struct{}{}
+		ch1 <- struct{}{}
+		<-ch9 //wait for the next loop
+		ch2 <- struct{}{}
+		ch2 <- struct{}{}
+		<-ch9 //wait for the next loop
+	}
 }
 
 func main() {
 	wg.Add(5)
+	go admin()
 	go worker1()
 	go worker2()
 	go worker3()
